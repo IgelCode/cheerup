7e04afe23eeae7bb7cd2710f81edea304f1a8240
IgelCode <tony.angermann@gmail.com>
2022-02-25 20:26:12 +0100

    transition

--- main.js
+++ main.js
@@ -5,6 +5,9 @@ const one = document.getElementById("one");
 const two = document.getElementById("two");
 const three = document.getElementById("three");
 const btns = document.getElementsByClassName("slidebtn");
+const btnone = document.getElementById("btnone");
+const btntwo = document.getElementById("btntwo");
+const btnthree = document.getElementById("btnthree");
 let i = 0;
 
 arrowleft.addEventListener("click", goleft);
@@ -38,25 +41,29 @@ myLoop();
 
 function goleft() {
   pics[i].style.opacity = "0%";
-
+  btns[i].style.color = "black";
   if (i > 0) {
     pics[i - 1].style.opacity = "100%";
+    btns[i - 1].style.color = "gold";
     i--;
   } else if (i == 0) {
     i = 2;
     pics[i].style.opacity = "100%";
+    btns[i].style.color = "gold";
   }
 }
 function goright() {
   pics[i].style.opacity = "0%";
+  btns[i].style.color = "black";
 
   if (i < pics.length - 1) {
     pics[i + 1].style.opacity = "100%";
-
+    btns[i + 1].style.color = "gold";
     i++;
   } else if (i == pics.length - 1) {
     i = 0;
     pics[i].style.opacity = "100%";
+    btns[i].style.color = "gold";
   }
 }
 
@@ -64,9 +71,10 @@ function btnstyle() {
   btnone.style.color = "black";
   btntwo.style.color = "black";
   btnthree.style.color = "black";
+  console.log(this);
   this.style.color = "gold";
   pics[i].style.opacity = "0%";
   i = Number(this.dataset.id);
-  console.log(this.dataset.id);
+
   pics[i].style.opacity = "100%";
 }
